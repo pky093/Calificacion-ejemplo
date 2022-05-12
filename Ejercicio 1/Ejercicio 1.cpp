11e5dc6fc6ce0254a54c8375a1149d21ce88a9e8
Edson Contreras <edson.contreras113@gmail.com>
2022-05-11 23:11:39 -0500

    modificación1

---
+++ Ejercicio 1/Ejercicio 1.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+void calificativo();
+//hola mundo 
+// hola mundo 2.0
+void notificar(char Calificativo)
+{
+    char sn;
+    switch (Calificativo)
+    {  
+    case'A':case'a':cout << "Bueno\n"; break;
+    case'B':case'b':cout << "Regular\n"; break;
+    case'C':case'c':cout << "Malo\n"; break;
+    case'D':case'd':cout << "Pesimo\n"; break;
+    default:
+    {
+        cout << "Inv\xa0lido. Desea Ingresar nuevamente s/S: "; cin >> sn;
+        if (sn == 'S' || sn == 's')
+            calificativo();
+    }
+    }
+
+}
+
+void calificativo()
+{
+    char Calif;
+    cout << "Ingrese Calificativo (A,B,C o D): ";
+    cin >> Calif;
+
+    notificar(Calif);
+}
+
+void main()
+{
+    calificativo();
+}
